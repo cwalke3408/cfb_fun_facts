b75885aecfad05cc7861af205ee872d23a48bcb2
unknown <chrsitw@g.clemson.edu>
2021-02-27 14:57:49 -0500

    first commit

---
+++ src/main/java/com/cfb/facts/cfbfunfacts/CfbFunFactsApplication.java
@@ -0,0 +1,13 @@
+package com.cfb.facts.cfbfunfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfbFunFactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfbFunFactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cfb/facts/cfbfunfacts/CfbFunFactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfb.facts.cfbfunfacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfbFunFactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
